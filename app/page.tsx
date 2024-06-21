7e0a8bd023b0ecea4aaab7b00ff2ae880e067e7b
Igwe Chinonso <igwechinonso77@gmail.com>
2024-06-21 22:17:29 +0100

    achievements section

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Container from "@/components/Container";
 import Banner from "@/components/Banner";
 import LogoCarousel from "@/app/sections/LogoCarousel";
 import NeedOurSolution from "@/app/sections/NeedOurSolution";
+import OurAchievements from "@/app/sections/OurAchievements";
 const Home = () => {
   return (
     <Container>
       <Banner />
       <LogoCarousel />
       <NeedOurSolution />
+      <OurAchievements />
     </Container>
   );
 }
---
+++ app/sections/OurAchievements/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type AchievementStatisticsProps = {
+  metric: string;
+  value: string;
+}
+const AchievementStatistics = ({ metric, value }: AchievementStatisticsProps) => {
+  return <div className='border-t border-white py-[2rem]'>
+    <p className='text-white md:text-[48px] text-[20px] font-medium mb-[1rem]'>{value}</p>
+    <p className='text-white text-[14px] md:text-[16px]'>{metric}</p>
+  </div>
+}
+const OurAchievements = () => {
+  return (
+    <div className='py-[1rem] md:py-[5rem] xl:px-[10rem] lg:px-[5rem] px-[1rem]  bg-primary-400'>
+      <div className='flex md:justify-between md:items-center items-start flex-col lg:flex-row justify-start'>
+        <p className='text-white basis-[50%] md:text-[40px] text-[20px] font-medium'>Our Achievements in Numbers</p>
+        <div className='flex-1'>
+          <p className='text-white lg:w-[70%] w-[100%]'>Turning satisfied clients into smiling faces is our ultimate metric for success.</p>
+        </div>
+      </div>
+      <div className='mt-[2rem] md:mt-[5rem] grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-1 md:gap-10'>
+        <AchievementStatistics metric="Happy Customers" value="1.5K" />
+        <AchievementStatistics metric="Years of Expertise" value="10+" />
+        <AchievementStatistics metric="Expert Team Members" value="60+" />
+        <AchievementStatistics metric="Customer Satisfaction Rate" value="100%" />
+      </div>
+    </div>
+  )
+}
+
+export default OurAchievements
